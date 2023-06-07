3d250858baf68796da771f2c4366980d7a6c2d9c
Alejandro <vaquero.alejandro19@gmail.com>
2023-06-07 12:14:00 +0200

    error solved

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import config as cfg
 from selenium import webdriver  
 from selenium.webdriver.common.by import By
 
-def checkDirectory(directoryToCreate):
-  return os.path.isdir(directoryToCreate)
+def obtenerImagenes():
+  chop = webdriver.ChromeOptions()
+  chop.add_extension(cfg.path_to_extension)
 
-def obtenerImagenes(chop):
   driver = webdriver.Chrome(cfg.PATH_WEB_DRIVER_EXE, chrome_options=chop)
   driver.get(cfg.url)
   driver.maximize_window()
@@ -20,8 +20,6 @@ def obtenerImagenes(chop):
 
   stylesDiv = driver.find_element(By.XPATH, cfg.styles)
   stylesImages = stylesDiv.find_elements(By.TAG_NAME, "img")
-
-  checkedAd = False
 
   #Make an infinite loop to create the wallpaper
   while True:
@@ -32,7 +30,11 @@ def obtenerImagenes(chop):
 
     #We put the random word on the input
     driver.find_element(By.XPATH, cfg.input).send_keys(wordSelected)
+    wordSelected = wordSelected.lower().replace(" ", "_")
+
     time.sleep(1)
+
+    checkAds(driver, False)
 
     isPremium = True
     while isPremium == True:
@@ -41,64 +43,74 @@ def obtenerImagenes(chop):
 
       #Select Type
       stylesImages[typeSelected].click()
+      time.sleep(1.5)
 
       try:
         driver.find_element(By.XPATH, cfg.adPremiumClose).click()
         isPremium = True
       except:
+        checkAds(driver, True)
         isPremium = False
+        driver.find_element(By.XPATH, cfg.createArtBtn).click()
 
-    if not checkedAd:
-      noAds = False
-      while noAds == False:
-        try:
-          driver.find_element(By.XPATH, cfg.adTwoClose).click()
-          noAds = True
-        except:
-          pass
-
-    #Press the create button
-    try:
-      driver.find_element(By.XPATH, cfg.createArtBtn).click()
-      noAds = True
-    except:
-      time.sleep(1.5)
-      driver.find_element(By.XPATH, cfg.adPremiumClose).click()
-
-    clickable = False
-
-    while clickable == False:
+    downloadable = False
+    while downloadable == False:
       try:
-          #Click on "Buy Print"
           driver.find_element(By.XPATH, cfg.downloadBtn).click()
-          clickable = True
+          downloadable = True
       except:
+          checkAds(driver, True)
           pass
       
     time.sleep(3)
     
     #We take the date and hour to put to the wallpaper name
     now = datetime.datetime.now()
-    formatFileName = "_" + now.strftime("%d %m %Y %H %M %S").replace(" ", "") + ".jpg"
+    formatFileName = now.strftime("_%d%m%Y%H%M%S.jpg")
+
     #We save the wallpaper on the directory path you selected on config.py
-    filename = wordSelected.lower().replace(" ", "_") + formatFileName
+    filename = wordSelected + formatFileName
 
-    fileToRename = cfg.downloadsDirectory + "\\" + "dream_TradingCard.jpg"
-    fileRenamed = cfg.downloadsDirectory + "\\" + wordSelected.lower().replace(" ", "_") + formatFileName
+    fileToRename = os.path.join(cfg.downloadsDirectory,"dream_TradingCard.jpg")
+    fileRenamed = os.path.join(cfg.downloadsDirectory, filename)
+
+    time.sleep(1)
+
     os.rename(fileToRename, fileRenamed)
-
-    directoryToCreate = cfg.directoryDownloads + wordSelected.replace(" ", "_")
-    if(checkDirectory(directoryToCreate)):
-      shutil.move(fileRenamed, os.path.join(directoryToCreate.lower(), filename))
-    else:
-      os.mkdir(directoryToCreate)
-      shutil.move(fileRenamed, os.path.join(directoryToCreate.lower(), filename))
+    moveDownload(fileRenamed, wordSelected, filename)    
     
     #Clear the input text to put the new word and start again
-    driver.find_element(By.XPATH, cfg.input).clear()
-    checkedAd = True
+    driver.find_element(By.XPATH, cfg.input).clear() 
 
-chop = webdriver.ChromeOptions()
-chop.add_extension(cfg.path_to_extension)
+def moveDownload(fileRenamed, wordSelected, filename):
+  directoryToCreate = os.path.join(cfg.directoryDownloads, wordSelected)
+  if(checkDirectory(directoryToCreate)):
+    shutil.move(fileRenamed, os.path.join(directoryToCreate.lower(), filename))
+  else:
+    os.mkdir(directoryToCreate)
+    shutil.move(fileRenamed, os.path.join(directoryToCreate.lower(), filename))
 
-obtenerImagenes(chop)
+def checkDirectory(directoryToCreate):
+  return os.path.isdir(directoryToCreate)
+
+def checkAds(driver: webdriver.Chrome, waitAttempts: bool):
+  attempts = 0
+  noAds = False
+  if waitAttempts:
+    while noAds == False and attempts < 15:
+      try:
+        driver.find_element(By.XPATH, cfg.adClose).click()
+        noAds = True
+      except:
+        pass
+      attempts += 1
+  else:
+    while noAds == False and attempts < 200:
+      try:
+        driver.find_element(By.XPATH, cfg.adClose).click()
+        noAds = True
+      except:
+        pass
+      attempts += 1
+
+obtenerImagenes()
